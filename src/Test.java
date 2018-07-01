b11cc51f7d7041c7ec2c8f40eec9b60c82614d8b
Mikołaj Klonowski <nick-93@o2.pl>
2018-07-01 20:14:51 +0200

    Z poprawkami

--- src/Test.java
+++ src/Test.java
@@ -1,22 +1,30 @@
-import  java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        Animals [] animal = new Animals[2];
-        animal[0] = new Animals(){};
-        animal[1] = new Animals(){};
+        Animals[] animal = new Animals[5];
+        boolean loop;
 
 
-        for (int i = 0; animal[0].equals(animal[1]); i++) {
-            System.out.println("Żle, tworzysz duplikat zwierząt");
-            animal[1] = new Animals(){};
+        for(int i=0; i<animal.length; i++){
+            do {
+                loop=true;
+                System.out.println("Wprowadź dane: " + (i+1) );
+                animal[i]= new Animals ();
+                for(int j=0; j<animal.length; j++){
+                    if (j!=i){
+                        if (animal[i].equals(animal[j]))
+                            loop=false;
+                    }
+                }
+
+                if (loop==false)
+                    System.out.println("Coś wpisałeś źle");
+            } while(!loop);
         }
+        for (Animals animals : animal) {
+            System.out.println (animals.toString () );
 
-
-        System.out.println(animal[0].toString());
-        System.out.println(animal[1].toString());
-
-
+        }
 
     }
 }
-
